56ad6966e8ad6f3d401d1674197afb5154274ff4
Vinitha <vinithabairagoni@gmail.com>
2026-03-08 16:12:09 +0530

    Update axios.js

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: "https://auth-frontend-project-y98r.onrender.com",
     withCredentials: true,
 });
 
